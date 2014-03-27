f55bf8807069a8aaf28155cd882730ed46e100c2
Dima Padalka <pdvxai@gmail.com>
2014-03-27 21:26:17 +0200

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,16 +18,28 @@ describe "User pages" do
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
   end
-end
-describe "with valid information" do
-  before do
-    fill_in "Name",         with: "Example User"
-    fill_in "Email",        with: "user@example.com"
-    fill_in "Password",     with: "foobar"
-    fill_in "Confirmation", with: "foobar"
-  end
+  describe "with valid information" do
+    before do
+      fill_in "Name",         with: "Example User"
+      fill_in "Email",        with: "user@example.com"
+      fill_in "Password",     with: "foobar"
+      fill_in "Confirmation", with: "foobar"
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
 
-  it "should create a user" do
-    expect { click_button submit }.to change(User, :count).by(1)
-  end
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        describe "followed by signout" do
+          before { click_link "Sign out" }
+          it { should have_link('Sign in') }
+        end
+      end
+    end
 end
+
+
+
+
+  end
